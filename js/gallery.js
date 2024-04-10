349fc3a224d693085d136eec95d231f087ed5082
JackxKlassen <156933678+JackxKlassen@users.noreply.github.com>
2024-04-10 14:33:45 -0500

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,54 +1,43 @@
-<!doctype html>
-	<html class="no-js" lang="en" dir="ltr">
-  		<head>
-  			<title>Assignment One</title>
-  			<link rel="stylesheet" href="css/style.css" media="all">
-  			
-  		</head>
-  		
-  	<nav>
-  			<a href="https://www.youtube.com/channel/UCYRJGa5NYNr5VVM4ZZM6JtA">My Youtube</a>
-  			<a href="list2.html">Best BBQ</a>
-  			<a href="list.html">Albums</a>
-  			<a href="index.html">Home</a>
-  			<a href="photo gallery.html">Photo Gallery</a>
-  	</nav>
+"use strict";
 
-	<body>
+var photoDivs = document.getElementsByClassName("gallery-image");
 
-		<h1>Jack Klassen</h1>
-		
-		<a href="jtkhbg@umsystem.edu">
-		
-			<img src="img/IMG_4084.jpeg"></a>
+var nextButton = document.getElementById("button_next");
 
+var previousButton = document.getElementById("button_previous");
 
-		<main>
-			<section>
-				<ul>
-					<li><a href="">Training Day</a></li>
-					<li><a href="">Se7en</a></li>
-					<li><a href="">The Shawshank Redemption</a></li>
-					<li><a href="">Good Will Hunting</a></li>
-					<li><a href="">Argo</a></li>
-				</ul>
+var photoNumber = document.getElementById("stepper");
 
+var currentPhotoNumber = 0;
 
-				<p>The list above is a list of my favorite movies of all time. The order of this list changes from time to time but the five movies tend to remain the same. Training Day is my favorite movie of all time because of the perfromace given by Denzel Washington</p>
-				<span></span>
-				<div></div>
-				<div></div>
+photoDivs[currentPhotoNumber].classList.remove('hideThis');
 
-				<p></p>
+photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
 
+nextButton.addEventListener('click', function() {
+  photoDivs[currentPhotoNumber].classList.add("hideThis");
+  currentPhotoNumber = currentPhotoNumber + 1;
 
-			</section>
+  if (currentPhotoNumber === photoDivs.length) {
+    currentPhotoNumber = 0;
+  }
 
-			<section></section>
+  photoDivs[currentPhotoNumber].classList.remove('hideThis');
 
-		</main>
+  photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
 
-	</body>
+});
 
-<footer></footer>
-</html>
+previousButton.addEventListener('click', function() {
+  photoDivs[currentPhotoNumber].classList.add("hideThis");
+  currentPhotoNumber = currentPhotoNumber - 1;
+
+  if (currentPhotoNumber < 0) {
+    currentPhotoNumber = photoDivs.length - 1;
+  }
+
+  photoDivs[currentPhotoNumber].classList.remove('hideThis');
+
+  photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
+
+});
